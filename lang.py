0282597e90d49395fbe811e128434324a7c25b38
CRUEL Project ™ <MOHAMADKHOSHNAVA@users.noreply.github.com>
2018-03-12 00:56:55 +0330

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,8 +1,12 @@
 fa = {
   "ingroup": {
-    "setowner":"کاربر [{}](tg://user?id={}) ادمین اصلی ربات در گروه شد👤",
+    "setowner": "کاربر [{}](tg://user?id={}) ادمین اصلی ربات در گروه شد👤",
+    "setlang": "زبان ربات به {} تنظیم شد"
   }
 }
 en = {
-  "test": "test"
+   "ingroup": {
+    "setowner": "user [{}](tg://user?id={}) promote to admin👤",
+    "setlang": "lang set to {}"
+  }
 }
